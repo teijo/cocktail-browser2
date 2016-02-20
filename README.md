750b92654b715daf1ab55030635be1b0407a11e2
Teijo Laine <teijo@aropupu.fi>
2016-02-21 00:21:00 +0200

    React + webpack base

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cocktail browser 2
+
+New iteration of cocktail browser with hopefully a less engineery UI and more
+modern development stack.
+
+
+## Development loop
+
+Run `npm start` to install dependencies and start live-reloading development
+server (prints out the port).
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: "./build",
+    publicPath: "/",
+    filename: "app.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: "babel",
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".less"]
+  },
+  plugins: [
+  ],
+  devServer: {
+    contentBase: "build",
+    progress: true,
+    inline: true,
+    historyApiFallback: true,
+    quiet: false,
+    noInfo: false,
+    host: "0.0.0.0",
+    port: 4000
+  }
+};
